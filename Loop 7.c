81ce07d1691026674336e7eb75e6a3fe4dea9bc4
Akila Thikshana <192502672+akilathikshana@users.noreply.github.com>
2025-01-16 00:56:56 +0530

    Create Loop 7.c

---
+++ Loop 7.c
@@ -0,0 +1,19 @@
+/*
+  * * * * * 
+* * * * 
+* * * 
+* * 
+*
+  */
+#include <stdio.h>
+int main(){
+    
+    for(int i=0; i < 5; ++i){
+        for(int k=5; k>i;k-- ){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
