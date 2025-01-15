7a72ee1cbcd87e46914e6577bcddc755394ee198
Akila Thikshana <192502672+akilathikshana@users.noreply.github.com>
2025-01-16 01:01:01 +0530

    Create Loop 11.c

---
+++ Loop 11.c
@@ -0,0 +1,40 @@
+/*
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     *
+*/
+#include <stdio.h>
+int main(){
+    
+    for(int i=0; i<5; i++){
+        
+        for(int k=0;k<5-i;k++){
+            printf(" ");
+        }
+        for(int j=0; j <= i ; ++j){
+            printf("* ");
+        }
+        
+        printf("\n");
+        
+    }
+        for(int i = 0; i < 5; ++i){
+        for(int j=0;j < i+1 ;j++){
+            printf(" ");
+        }
+        for(int k = 5; k > i; --k){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
